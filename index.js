291128f6047e2ad2df7d10d3fbadd0773ba4f6f8
Gil Solis <gilsolisuw@gmail.com>
2020-07-14 21:00:36 -0700

    button variables

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 let url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+var xhrbtn = document.querySelector("#xhr");
+var fetchbtn = document.querySelector("#fetch");
+var jquerybtn = document.querySelector("#jquery");
+var axiosbtn = document.querySelector('axios');
+
