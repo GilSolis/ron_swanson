29432dff403d0f43c84410d88af45a1786e9913d
Gil Solis <gilsolisuw@gmail.com>
2020-07-14 21:17:15 -0700

    xhr and fetch

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ var fetchbtn = document.querySelector("#fetch");
 var jquerybtn = document.querySelector("#jquery");
 var axiosbtn = document.querySelector('axios');
 
+var display = document.querySelector("#quote")
+
+xhrbtn.addEventListener("click", function () {
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState == 4 && XHR.status == 200) {
+            var quote = JSON.parse(XHR.responseText)[0];
+            display.innerText = quote;
+            console.log(quote)
+        }
+    }
+    XHR.open("GET", url)
+    XHR.send();
+})
+
+fetchbtn.addEventListener("click", function () {
+    fetch(url)
+        .then(function (req) {
+            req.json().then(function (data) {
+                display.innerText = data[0]
+            })
+        })
+        .catch(function () {
+            alert("Error!")
+        })
+})
+
+jquerybtn.addEventListener("click", function () {
